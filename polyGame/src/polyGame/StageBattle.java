b6def20bde7e420d513be29bd76d8566eaf7f8f7
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 17:59:50 +0900

    Add: StageBattle Class(playAttack Method)

--- polyGame/src/polyGame/StageBattle.java
+++ polyGame/src/polyGame/StageBattle.java
@@ -5,12 +5,16 @@ import java.util.Vector;
 
 public class StageBattle extends Stage {
 	private Random ran = new Random();
+	
 	private UnitManager unitManager;
 	private Vector<Player> playList;
 	private Vector<Unit> unitList;
 	
 	private int unitDie;
 	private int playerDie;
+	
+	private final int ATTACK = 1;
+	private final int SKILL = 2;
 	
 	public StageBattle() {
 		unitManager = new UnitManager();
@@ -48,6 +52,34 @@ public class StageBattle extends Stage {
 		
 		for(int i = 0; i < unitList.size(); i ++) {
 			unitList.get(i).printData();
+		}
+	}
+	
+	// playAttack
+	public void playAttack(int index) {
+		Player play = playList.get(index);
+		
+		if(play.hp <= 0) {
+			return;
+		}
+		
+		System.out.println("=== [MENU] ===");
+		System.out.printf("[%s] [1]공격 [2]스킬\n", play.name);
+		
+		int sel = GameManager.scan.nextInt();
+		if(sel == ATTACK) {
+			while(true) {
+				int idx = ran.nextInt(unitList.size());
+				
+				Unit unit = unitList.get(idx);
+				if(unit.hp > 0) {
+					play.attack(unit);
+					break;
+				}
+			}
+		}
+		else if(sel == SKILL) {
+			
 		}
 	}
 	
