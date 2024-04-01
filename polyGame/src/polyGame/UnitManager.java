86dce7553053e24c6c68f6432895a27255057475
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 17:15:06 +0900

    Modify: UnitManager Class(name change)

--- polyGame/src/polyGame/UnitManager.java
+++ polyGame/src/polyGame/UnitManager.java
@@ -6,16 +6,16 @@ import java.util.Vector;
 public class UnitManager {
 	private Random ran = new Random();
 	
-	private Vector<Player> playList = new Vector<>();
-	private Vector<Unit> unitList = new Vector<>();
+	public Vector<Player> play_List = new Vector<>();
+	public Vector<Unit> unit_List = new Vector<>();
 	
 	private String path = "polyGame";
 	private String unit[] = {"UnitWolf", "UnitOrc", "UnitBat", "UnitDragon"};
 	
 	public UnitManager() {
-		playList.add(new Player("팔라딘", 1000, 50));
-		playList.add(new Player("나이트로드", 400, 100));
-		playList.add(new Player("비숍", 700, 40));
+		play_List.add(new Player("팔라딘", 1000, 50));
+		play_List.add(new Player("나이트로드", 400, 100));
+		play_List.add(new Player("비숍", 700, 40));
 	}
 	
 	private void unitRanSet(int size) {
@@ -30,7 +30,7 @@ public class UnitManager {
 				int power = ran.nextInt(30) + 10;
 				
 				temp.init(hp, power);
-				unitList.add(temp);
+				unit_List.add(temp);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
