cbd8299503d2bff6947fdd29d56d12a1185f3981
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 18:28:17 +0900

    Add: UnitBat Class

--- polyGame/src/polyGame/UnitBat.java
+++ polyGame/src/polyGame/UnitBat.java
@@ -1,5 +1,11 @@
 package polyGame;
 
-public class UnitBat {
-
+public class UnitBat extends Unit {
+	public UnitBat() {
+		name = "스티지";
+	}
+	
+	public void skill() {
+		System.out.println("적 한명에게 침묵 시전");
+	}
 }
