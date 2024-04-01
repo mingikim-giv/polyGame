888ba1ba7dff89d9d85a05c26f381f006d1d41da
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 17:19:24 +0900

    Add: StageBattle Class(init Method)

--- polyGame/src/polyGame/StageBattle.java
+++ polyGame/src/polyGame/StageBattle.java
@@ -20,16 +20,24 @@ public class StageBattle extends Stage {
 		playerDie = 0;
 	}
 	
+	// init
+	public void init() {
+		unitManager.unit_List.clear();
+		unitManager.unitRanSet(4);
+		
+		playList = null;
+		playList = unitManager.play_List;
+		
+		unitList = null;
+		unitList = unitManager.unit_List;
+		
+		unitDie = unitList.size();
+		playerDie = playList.size();
+	}
+	
 	@Override
 	public boolean update() {
 		// TODO Auto-generated method stub
 		return false;
 	}
-
-	@Override
-	public void init() {
-		// TODO Auto-generated method stub
-		
-	}
-
 }
