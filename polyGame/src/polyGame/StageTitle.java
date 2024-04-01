16469f1b4e8714543dcc2c1a7bd9b95f45d84553
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 15:57:10 +0900

    Add: StageTitle Class(override update, init)

--- polyGame/src/polyGame/StageTitle.java
+++ polyGame/src/polyGame/StageTitle.java
@@ -1,5 +1,24 @@
 package polyGame;
 
-public class StageTitle {
+public class StageTitle extends Stage {
 
+	@Override
+	public boolean update() {
+		System.out.println("=== Poly RPG GAME ===");
+		System.out.println("[시작]을 입력하세요");
+		String start = GameManager.scan.next();
+		
+		if(start.equals("시작")) {
+			
+		}
+		GameManager.nextStage = "LOBBY";
+		return false;
+	}
+
+	@Override
+	public void init() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
