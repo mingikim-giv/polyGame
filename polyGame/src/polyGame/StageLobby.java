55eb906bb2c7d29f79e130eb0047a5c027a0e425
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 16:17:56 +0900

    Modify: StageLobby Class(override update, init)

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -1,10 +1,21 @@
 package polyGame;
 
 public class StageLobby extends Stage {
-
+	private final int BATTLE = 1;
+	private final int END = 2;
+	
 	@Override
 	public boolean update() {
-		// TODO Auto-generated method stub
+		System.out.println("=== LOBBY ===");
+		System.out.println("[1]전투 [2]종료");
+		int sel = GameManager.scan.nextInt();
+		
+		if(sel == BATTLE) {
+			GameManager.nextStage = "BATTLE";
+		}
+		else if(sel == END) {
+			GameManager.nextStage = "";
+		}
 		return false;
 	}
 
