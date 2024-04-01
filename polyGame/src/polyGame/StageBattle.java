139bdc77be26e673b89e7a2cce76ffd15d1e158d
mingikim-giv <mingikim.giv@gmail.com>
2024-04-01 16:35:27 +0900

    Add: StageBattle Class(producers)

--- polyGame/src/polyGame/StageBattle.java
+++ polyGame/src/polyGame/StageBattle.java
@@ -1,7 +1,25 @@
 package polyGame;
 
-public class StageBattle extends Stage {
+import java.util.Random;
+import java.util.Vector;
 
+public class StageBattle extends Stage {
+	private Random ran = new Random();
+	private UnitManager unitManager;
+	private Vector<Player> playList;
+	private Vector<Unit> unitList;
+	
+	private int unitDie;
+	private int playerDie;
+	
+	public StageBattle() {
+		unitManager = new UnitManager();
+		playList = null;
+		unitList = null;
+		unitDie = 0;
+		playerDie = 0;
+	}
+	
 	@Override
 	public boolean update() {
 		// TODO Auto-generated method stub
