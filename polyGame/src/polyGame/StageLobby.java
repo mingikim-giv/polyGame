8875889b4df2383d71f324a8e743ed1532d5c898
mingikim-giv <mingikim.giv@gmail.com>
2024-04-02 11:10:51 +0900

    Modify: StageLobby Class(name change)

--- polyGame/src/polyGame/StageLobby.java
+++ polyGame/src/polyGame/StageLobby.java
@@ -6,7 +6,7 @@ public class StageLobby extends Stage {
 	
 	@Override
 	public boolean update() {
-		System.out.println("=== LOBBY ===");
+		System.out.println("===[DUNGEON LOBBY]===");
 		System.out.println("[1]전투 [2]종료");
 		int sel = GameManager.scan.nextInt();
 		
