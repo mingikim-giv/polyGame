317dd044d0e862af477953da3fc403492a80ce50
mingikim-giv <mingikim.giv@gmail.com>
2024-04-02 15:14:40 +0900

    Modify: PaladinSkill Class(name change)

--- polyGame/src/polyGame/PaladinSkill.java
+++ polyGame/src/polyGame/PaladinSkill.java
@@ -4,7 +4,7 @@ public class PaladinSkill extends Skill {
 
 	@Override
 	public void skill(Unit unit) {
-		String name = "그랜드 크로스!";
+		String name = "세이크리드 바스티온!";
 		System.out.printf("%s님 %s시전\n", Paladin.instance.name, name);
 		System.out.printf("%s으로 공격 시 %s의 2배로 공격하고 50%확률로 기절\n", name, Paladin.instance.name);
 		
