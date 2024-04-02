adebd323c97edef1e0444ea50218e4bc56fe45c4
mingikim-giv <mingikim.giv@gmail.com>
2024-04-02 14:36:00 +0900

    Add: Paladin, Bishop, NightLord Class

---
+++ polyGame/src/polyGame/Bishop.java
@@ -0,0 +1,13 @@
+package polyGame;
+
+public class Bishop extends Unit {
+	public static Bishop instance = new Bishop();
+	
+	  public Unit add() {
+		  this.name = "비숍";
+		  this.maxHp = 400;
+		  this.power = 40;
+		  Unit temp = new Unit(name, maxHp, power);
+		  return temp;
+	  }
+}
---
+++ polyGame/src/polyGame/NightLord.java
@@ -0,0 +1,13 @@
+package polyGame;
+
+public class NightLord extends Unit {
+	public static NightLord instance = new NightLord();
+	
+	  public Unit add() {
+		  this.name = "나이트로드";
+		  this.maxHp = 300;
+		  this.power = 150;
+		  Unit temp = new Unit(name, maxHp, power);
+		  return temp;
+	  }
+}
