b91304f9afe259eb36c8140a6d6dceae51e97521
mingikim-giv <mingikim.giv@gmail.com>
2024-04-03 15:27:17 +0900

    Modify: Unit Class

--- polyGame/src/polyGame/UnitDragon.java
+++ polyGame/src/polyGame/UnitDragon.java
@@ -3,21 +3,21 @@ package polyGame;
 public class UnitDragon extends Unit {
 	
 	public UnitDragon() {
-		name = "혼테일";
+		setName("혼테일");
 	}
 	
 	public void skill(Unit unit) {
 		String name = "브레스🔥";
-		System.out.printf("[스킬]%s님 %s시전\n", this.name, name);
-		System.out.printf("[스킬 정보]%s으로 공격 시 %s의 2배 공격\n", name, this.power);
+		System.out.printf("[스킬]%s님 %s시전\n", this.getName(), name);
+		System.out.printf("[스킬 정보]%s으로 공격 시 %s의 2배 공격\n", name, this.getPower());
 		
-		unit.setHp(unit.getHp() - this.power*2);
+		unit.setHp(unit.getHp() - this.getPower()*2);
 		if(unit.getHp() <= 0) {
 			unit.setHp(0);
 			System.out.printf("%s 사망\n", unit.getName());
 		}
 		
-		System.out.printf("%s가 %s시전 %s[HP:%d]\n", this.name, name, unit.getName(), unit.getHp());
+		System.out.printf("%s가 %s시전 %s[HP:%d]\n", this.getName(), name, unit.getName(), unit.getHp());
 		
 		try {
 			Thread.sleep(1000);
--- polyGame/src/polyGame/UnitSeren.java
+++ polyGame/src/polyGame/UnitSeren.java
@@ -3,21 +3,21 @@ package polyGame;
 public class UnitSeren extends Unit {
 	
 	public UnitSeren() {
-		name = "세렌";
+		setName("세렌");
 	}
 	
 	public void skill(Unit unit) {
 		String name = "미트라의 권능☀️";
-		System.out.printf("[스킬]%s님 %s시전\n", this.name, name);
-		System.out.printf("[스킬 정보]%s으로 공격 시 %s의 3배 공격\n", name, this.power);
+		System.out.printf("[스킬]%s님 %s시전\n", this.getName(), name);
+		System.out.printf("[스킬 정보]%s으로 공격 시 %s의 3배 공격\n", name, this.getPower());
 		
-		unit.setHp(unit.getHp() - this.power*3);
+		unit.setHp(unit.getHp() - this.getPower()*3);
 		if(unit.getHp() <= 0) {
 			unit.setHp(0);
 			System.out.printf("%s 사망\n", unit.getName());
 		}
 		
-		System.out.printf("%s가 %s시전 %s[HP:%d]\n", this.name, name, unit.getName(), unit.getHp());
+		System.out.printf("%s가 %s시전 %s[HP:%d]\n", this.getName(), name, unit.getName(), unit.getHp());
 		
 		try {
 			Thread.sleep(1000);
