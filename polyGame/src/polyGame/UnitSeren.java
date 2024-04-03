32624566ba8a0208796526cae236e2e6c4758405
mingikim-giv <mingikim.giv@gmail.com>
2024-04-03 11:12:38 +0900

    Add: UnitSeren Class(skill)

--- polyGame/src/polyGame/UnitSeren.java
+++ polyGame/src/polyGame/UnitSeren.java
@@ -5,4 +5,24 @@ public class UnitSeren extends Unit {
 	public UnitSeren() {
 		name = "세렌";
 	}
+	
+	public void skill(Unit unit) {
+		String name = "미트라의 권능☀️";
+		System.out.printf("%s님 %s시전\n", this.name, name);
+		System.out.printf("%s으로 공격 시 %s의 2배 공격\n", name, this.name);
+		
+		unit.setHp(this.power*2);
+		if(unit.getHp() <= 0) {
+			unit.setHp(0);
+			System.out.printf("%s 사망\n", unit.getName());
+		}
+		
+		System.out.printf("%s님 %s시전 %s[HP:%d]\n", this.name, name, unit.getName());
+		
+		try {
+			Thread.sleep(1000);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
